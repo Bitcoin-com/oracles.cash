4655da45b58031d1ff68cad66951716a13ff66bc
Gabriel Cardona <gabriel@bitcoin.com>
2019-09-21 15:46:26 -0600

    Renaming.

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,11 +5,11 @@ const router = express.Router()
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("swagger-v2")
+  res.render("swagger-v1")
 })
 
 router.get("/v2", (req, res, next) => {
-  res.render("swagger-v2")
+  res.render("swagger-v1")
 })
 
 module.exports = router
